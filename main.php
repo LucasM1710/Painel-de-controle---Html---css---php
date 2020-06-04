7b0fc772edb5cd362c6346df7ca3cffdf3279d19
Lucas Matheus <Lucasmatheusc@hotmail.com>
2020-06-04 19:40:50 -0300

    Logout e mais HTML/CSS

--- main.php
+++ main.php
@@ -1,1 +1,30 @@
-<h2>Bem vindo ao painel<?php echo $_SESSION['user'];?></h2>
+<?php
+	if(isset($_GET['loggout'])){
+		Painel::loggout();
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Painel de Controle</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content= "width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>estilo/all.css" > 
+	<link href="<?php echo INCLUDE_PATH_PAINEL?>css/style.css" rel="stylesheet"/>
+
+</head>
+
+<body>
+	<div class="menu"></div>
+	<header>
+		<div class="center">
+			<div class="loggout">
+				<a href="<?php echo INCLUDE_PATH_PAINEL?>?loggout"><i class="fas fa-sign-out-alt"></i></a>
+			</div><!--Loggout-->
+			<div class="clear"></div>
+		</div>
+	</header>
+</body>
+
+</html>
