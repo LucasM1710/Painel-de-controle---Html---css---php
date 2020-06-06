6e88df8753f60b3163378fdfc1f9515c21b68827
Lucas Matheus <Lucasmatheusc@hotmail.com>
2020-06-06 06:58:08 -0300

    Iniciando Sistema de Usuário e validação de cadastro no banco de dados

--- main.php
+++ main.php
@@ -37,7 +37,7 @@
 		</div><!--box-usuario-->
 		<div class ="items-menu">
 			<h2>Cadastro</h2>
-			<a href="">Cadastrar Depoimento</a>
+			<a href="<?php echo INCLUDE_PATH_PAINEL?>cadastrar-depoimento">Cadastrar Depoimento</a>
 			<a href="">Cadastrar Serviço</a>
 			<a href="">Cadastrar Slides</a>
 			<h2>Gestão</h2>
@@ -57,55 +57,17 @@
 			<div class="menu-btn">
 				<i class="fa fa-bars"></i>
 			</div><!--menu-btn-->
+
 			<div class="loggout">
+				<a href="<?php echo INCLUDE_PATH_PAINEL?>"><i class="fa fa-home"></i><span>Página Inicial</span></a>
 				<a href="<?php echo INCLUDE_PATH_PAINEL?>?loggout"><i class="fas fa-sign-out-alt"></i> <span>Sair </span></a>
 			</div><!--Loggout-->
+
 			<div class="clear"></div>
 		</div>
 	</header>
 	<div class="content">
-		<div class="box-content left w100">	
-			<h2><i class="fa fa-home"></i> Painel de Controle - <?php echo $nomeEmpresa?></h2>
-
-			<div class="box-metricas">
-				<div class="box-metrica-single">
-					<div class="box-metrica-wraper">
-						<h2>Usuários Online</h2>
-						<p>10</p>
-					</div><!--box-metrica-wraper-->
-				</div><!--box-metrica-single-->
-
-				<div class="box-metrica-single">
-					<div class="box-metrica-wraper">
-						<h2>Total de Visitas</h2>
-						<p>100</p>
-					</div><!--box-metrica-wraper-->
-				</div><!--box-metrica-single-->
-
-				<div class="box-metrica-single">
-					<div class="box-metrica-wraper">
-						<h2>Visitas Hoje</h2>
-						<p>3</p>
-					</div><!--box-metrica-wraper-->
-				</div><!--box-metrica-single-->
-			</div><!--box-metricas-->
-
-		</div><!--box-content-->
-
-		<!--
-		<div class="box-content left w100">
-			
-		</div>
-
-		<div class="box-content left w50">
-			
-		</div>
-
-		<div class="box-content right w50">
-			
-		</div>
-		-->
-		<div class="clear"></div>
+		<?php Painel::carregarPagina();?>
 
 	</div><!--content-->
 
---
+++ pages/home.php
@@ -0,0 +1,57 @@
+<div class="box-content w100">	
+	<h2><i class="fa fa-home"></i> Painel de Controle - <?php echo NOME_EMPRESA ?></h2>
+
+	<div class="box-metricas">
+		<div class="box-metrica-single">
+			<div class="box-metrica-wraper">
+				<h2>Usuários Online</h2>
+				<p>10</p>
+			</div><!--box-metrica-wraper-->
+		</div><!--box-metrica-single-->
+
+		<div class="box-metrica-single">
+			<div class="box-metrica-wraper">
+				<h2>Total de Visitas</h2>
+				<p>100</p>
+			</div><!--box-metrica-wraper-->
+		</div><!--box-metrica-single-->
+
+		<div class="box-metrica-single">
+			<div class="box-metrica-wraper">
+				<h2>Visitas Hoje</h2>
+				<p>3</p>
+			</div><!--box-metrica-wraper-->
+		</div><!--box-metrica-single-->
+	</div><!--box-metricas-->
+</div><!--box-content-->
+
+<div class="box-content w100">	
+	<h2><i class="fas fa-user-friends"></i> Usuários Online</h2>
+	<div class="table-responsive">
+		<div class="row">
+			<div class="col">
+				<span>IP</span>
+			</div><!--col-->
+			<div class="col">
+				<span>Última Ação</span>
+			</div><!--col-->
+			<div class="clear"></div>
+		</div><!--row-->
+		<?php
+			for ($i=0; $i < 10; $i++) { 
+	0		
+		?>
+
+		<div class="row">
+			<div class="col">
+				<span>199.999.999.999</span>
+			</div><!--col-->	
+			<div class="col">
+				<span>02/06/2020 19:00:00</span>
+			</div><!--col-->
+			<div class="clear"></div>
+		</div><!--row-->
+		<?php }?>
+	</div><!--table-responsive-->
+
+</div><!--box-content-->
