1a4aa0e8ec373074b5b8768e94e8727da3a13c76
Lucas Matheus <Lucasmatheusc@hotmail.com>
2020-06-07 12:45:42 -0300

    Editando usuários com upload

--- pages/editar-usuario.php
+++ pages/editar-usuario.php
@@ -5,7 +5,38 @@
 		<?php 
 			if(isset($_POST['acao'])){
 				//Enviei o meu formulário
-				Painel::alert('sucesso',' Atualizado com sucesso!');
+				$nome = $_POST['nome'];
+				$senha = $_POST['password'];
+				$imagem = $_FILES['imagem'];
+				$imagem_atual = $_POST['imagem_atual'];
+				$usuario = new Usuario();
+				if($imagem['name'] != ''){
+					//Existe o upload de imagem
+					
+					if(Painel::imagemValida($imagem)){
+						Painel::deleteFile($imagem_atual);
+						$imagem = Painel::uploadFile($imagem);
+
+					if($usuario->atualizarUsuario($nome,$senha,$imagem)){
+						$_SESSION['img'] = $imagem;
+						Painel::alert('sucesso',' Atualizado com sucesso junto com a imagem!');
+					}else{
+						Painel::alert('erro',' Ocorreu um erro ao atualizar...junto com a imagem');
+					}
+				}else{
+						Painel::alert('erro',' O formato da imagem não é valido');
+					}
+				
+
+				}else{
+					$imagem = $imagem_atual;
+				
+					if($usuario->atualizarUsuario($nome,$senha,$imagem)){
+						Painel::alert('sucesso',' Atualizado com sucesso!');
+					}else{
+						Painel::alert('erro',' Ocorreu um erro ao atualizar...');
+					}
+				}
 			}
 
 		?>
@@ -18,7 +49,7 @@
 
 		<div class="form-group">
 			<label>Senha: </label>
-			<input type="password" name="nome" value="<?php echo $_SESSION['password']; ?>" required>
+			<input type="password" name="password" value="<?php echo $_SESSION['password']; ?>" required>
 		</div><!--form-group-->
 
 		<div class="form-group">
