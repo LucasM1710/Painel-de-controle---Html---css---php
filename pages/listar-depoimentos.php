356345ef0af72e052af3c53fcf42143349f1cf72
Lucas Matheus <Lucasmatheusc@hotmail.com>
2020-06-09 21:05:54 -0300

    Ordenando Itens

--- pages/listar-depoimentos.php
+++ pages/listar-depoimentos.php
@@ -4,10 +4,12 @@
 		$idExcluir = (int)$_GET['delete'];
 		Painel::deletar('tb_site.depoimentos',$idExcluir);
 		Painel::redirect(INCLUDE_PATH_PAINEL.'listar-depoimentos');
+	}else if(isset($_GET['order']) && isset($_GET['id'])){
+		Painel::orderItem('tb_site.depoimentos',$_GET['order'],$_GET['id']);
 	}
 
 	$paginaAtual = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
-	$porPagina = 4;
+	$porPagina = 8;
 
 	$depoimentos = Painel::selectAll('tb_site.depoimentos',($paginaAtual - 1) * $porPagina, $porPagina);
 
@@ -22,6 +24,8 @@
 			<td><i class="far fa-calendar-alt"></i> Data</td>
 			<td><i class="far fa-edit"></i> Editar</td>
 			<td><i class="fas fa-eraser"></i> Deletar</td>
+			<td>#</td>
+			<td>#</td>
 		</tr>
 
 		<?php
@@ -33,6 +37,8 @@
 			<td><?php echo $value ['data']; ?></td>
 			<td><a class="btn edit" href="<?php echo INCLUDE_PATH_PAINEL ?>editar-depoimento?id=<?php echo $value['id'];?>">Editar</a></td>
 			<td><a actionBtn="delete" class="btn delete" href="<?php echo INCLUDE_PATH_PAINEL?>listar-depoimentos?delete=<?php echo $value['id']; ?>">Deletar</a></td>
+			<td><a class="btn order" href="<?php echo INCLUDE_PATH_PAINEL?>listar-depoimentos?order=up&id=<?php echo $value['id']?>"><i class="fa fa-angle-up"></i></a></td>
+			<td><a class="btn order"href="<?php echo INCLUDE_PATH_PAINEL?>listar-depoimentos?order=down&id=<?php echo $value['id']?>"><i class="fa fa-angle-down"></i></a></td>
 		</tr>
 
 		<?php }?>
